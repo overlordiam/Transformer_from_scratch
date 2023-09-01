6990633d89f2c2ff14e38c0ecbd150031f8186b0
Suhaas Gummalam <footballandgermany@gmail.com>
2023-08-31 23:43:04 -0700

    add MultiHead ResidualConnections EncoderBlock and Encoder classes

--- model.py
+++ model.py
@@ -74,3 +74,104 @@ class FeedForward(nn.Module):
         # apply feedforward layer to the input
         return self.linear2(self.dropout(self.relu(self.linear1(x))))
     
+
+class MutliHeadAttention(nn.Module):
+    def __init__(self, h: int, d_model: int, dropout: float):
+        super().__init__()
+        self.d_model = d_model
+        self.h = h
+
+        # check whether the tensor can be split into h equal parts
+        assert d_model % h == 0, "d_model not divisible by h"
+        
+        self.d_k = d_model // h
+
+        # initialize weights for query, key and value
+        self.w_q = nn.Linear(d_model, d_model, bias=False)
+        self.w_k = nn.Linear(d_model, d_model, bias=False)
+        self.w_v = nn.Linear(d_model, d_model, bias=False)
+        self.w_o = nn.Linear(d_model, d_model, bias=False)
+
+        self.dropout = nn.Dropout(dropout)
+
+    @staticmethod
+    def attention(query, key, value, mask, dropout: nn.Dropout):
+        # calculate the attention for the given chunks of query, key and value
+        d_k = query.shape[-1]
+        attention_scores = (query @ key.transpose(-2,-1)) / math.sqrt(d_k)
+
+        # apply -1e9 to any position of tensor where value == 0
+        if mask is not None:
+            attention_scores.masked_fill_(mask == 0, -1e9)
+
+        # apply softmax to the last dimension of input
+        attention_scores = attention_scores.softmax(dim=-1)
+
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+
+        return attention_scores @ value, attention_scores        
+
+    def forward(self, query, key, value, mask):
+        
+        # calculate weighted values of query, value and key 
+        query = self.w_q(query)
+        key = self.w_k(key)
+        value = self.w_v(value)
+
+        # split the q, k and v values into h parts and apply attention
+        query = query.view(query.shape[0], self.h, query.shape[1], self.d_k)
+        key = query.view(key.shape[0], self.h, key.shape[1], self.d_k)
+        value = value.view(value.shape[0], self.h, value.shape[1], self.d_k)
+
+        # calculate the word representation with position and context (attention)
+        x, self.attention_scores = MutliHeadAttention().attention(query, key, value, mask, self.dropout)
+
+        # combine the heads to form the original matrix
+        x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.h*self.d_k) # shape -> (batch, seq_len, d_model)
+
+        return self.w_o(x)
+    
+
+class ResidualConnection(nn.Module):
+    def __init__(self, dropout: float):
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.layer_norm = LayerNorm()
+
+    def forward(self, x, sublayer):
+        # output of sub layer = LayerNorm(x + Sublayer(x)) 
+        return self.dropout(self.norm(x + sublayer(x)))
+    
+
+class EncoderBlock(nn.Module):
+    # Assemble both the sub layers and apply layer norm to finalize the encoder block
+    def __init__(self, self_attention_block: MutliHeadAttention, feed_forward_block:FeedForward, dropout:float):
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connections = nn.ModuleList([ResidualConnection(dropout) for _ in range(2)])
+
+    def forward(self, x, mask):
+        # We apply lambda function so that the MutliHeadAttention object executes inside the residual function,
+        # which gives data flow. The feed_forward_block does not need the input as it feeds it inside the ResidualConnection class 
+        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, mask))
+        x = self.residual_connections[1](x, self.feed_forward_block)
+        return x
+
+
+class Encoder(nn.Module):
+    # handles the N stacks of the encoder. Output of each stack is fed in as the input to the next stack.
+    def __init__(self, layers: nn.ModuleList):
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNorm()
+
+    def forward(self, x, mask):
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
+        
+    
+
+    
